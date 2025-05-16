e238576938aa9029434bf0e49377123e8e9dc9f2
Kulbhushan <kulbhushan.mayer@gmail.com>
2025-05-16 11:11:56 +0530

    fixed code issue

--- main.tf
+++ main.tf
@@ -11,4 +11,4 @@ resource "aws_instance" "aws_instance" {
   ami                           = "ami-0e62a7626e25342b2"
   instance_type                 = "t2.micro"
   key_name                      = aws_key_pair.keypair.key_name
-]
+}
