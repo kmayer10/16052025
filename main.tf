05577c7d795c8e6ad19bec9d0cd3356a6baa72d2
Kulbhushan <kulbhushan.mayer@gmail.com>
2025-05-16 11:11:24 +0530

    added code block for EC2

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "aws_key_pair" "keypair" {
   public_key = tls_private_key.keypair.public_key_openssh
   tags       = local.tags
 }
+resource "aws_instance" "aws_instance" {
+  ami                           = "ami-0e62a7626e25342b2"
+  instance_type                 = "t2.micro"
+  key_name                      = aws_key_pair.keypair.key_name
+]
